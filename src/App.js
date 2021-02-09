4b5d09cced934556cccff1b879c4b0b8f7ec7082
w1ndfury <w1ndfury216@gmail.com>
2021-02-09 21:29:19 +0200

    Fix Hidding and Buttons

--- src/App.js
+++ src/App.js
@@ -4,28 +4,18 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import MyParticles from './components/MyParticles'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Language from './components/Language'
+import Intro from './components/Intro'
 
 function App() {
     return (
         <div className="my_app">
-        
-            <div class="container-fluid px-0">
-                <div class="row no-gutters">
-                    <div class="col-sm-3">
-                        <MyParticles />
-                        <Language/>
+            <MyParticles />        
+            <div class="container-fluid px-0">              
+                <div class="row no-gutters">                 
+                    <div class="col-sm-12">                           
+                            <Intro/>
                     </div>
-                    <div class="col-sm-6">
-                            <MyParticles />
-                            <Header/>
-                    </div>
-                    <div class="col-sm-3">
-                        <MyParticles />
-                        <Language/>
-                    </div>
+
                 </div>
             </div>
         </div>
